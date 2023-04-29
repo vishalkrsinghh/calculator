e23a8d174de1ed6328908686d3b9e94b5635556e
vishal kumar singh <115806971+vishalkrsinghh@users.noreply.github.com>
2023-04-29 18:45:17 +0530

    sec commit

---
+++ script.cjs
@@ -0,0 +1,39 @@
+let btn = document.getElementsByClassName("btn");
+let display = document.getElementsByClassName("display")[0];
+// console.log(btn);
+let displayValue = "";
+
+let clrr = (displayValue) => {
+    displayValue = displayValue.slice(0, displayValue.length - 1);
+    return displayValue;
+}
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+    
+        if (btn[i].value == "a/c") {
+            displayValue = "";
+            display.innerHTML = "";
+        }
+        else if (btn[i].value == "=") {
+            evaluatee = (displayValue) => {
+                // console.log(displayValue);
+                display.innerHTML = eval(displayValue);
+                // return displayValue;
+            }
+            evaluatee(displayValue);
+            displayValue = eval(displayValue);
+            // console.log(displayValue);
+        }
+        else if (btn[i].value == "clr") {
+            // console.log(displayValue);
+            displayValue = clrr(displayValue);
+            display.innerHTML = displayValue;
+        }
+        else {
+            displayValue += btn[i].value;
+            display.innerHTML = displayValue;
+        }
+    })
+}
+console.log(displayValue);
